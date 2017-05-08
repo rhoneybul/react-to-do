369ef5a2a4e42fea7a3cbda687076fe125e1aa3e
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-05-09 07:28:13 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+	if(req.headers['x-forwarded-proto'] == 'https'){
+		res.redirect('http://' + req.hostname + req.url);
+	} else{
+		next();
+	}
+})
+
+app.listen(PORT, function() {
+	console.log("Express Server is up on port: " + PORT);
+});
